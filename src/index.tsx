6127ef3bd996b8b49c1b701cd8bdcf2658a39dc3
roottool <smart.dog.lilienthal@gmail.com>
2018-11-04 14:34:43 +0900

    create-react-appのバージョンアップ対応

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -10,4 +9,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
