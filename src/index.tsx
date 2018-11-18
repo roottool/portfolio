8399c8c36bfb246e94bf70b300e4a0a1f1aec432
roottool <smart.dog.lilienthal@gmail.com>
2018-11-18 17:31:58 +0900

    不要なパッケージを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-import 'bootstrap/dist/css/bootstrap.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
