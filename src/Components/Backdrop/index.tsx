4a013be317159cf1402a59836a0d555329dca7c0
roottool <smart.dog.lilienthal@gmail.com>
2018-12-02 21:42:24 +0900

    サイドメニュー表示時の影部分をstyled-components化

--- src/Components/Backdrop/index.tsx
+++ src/Components/Backdrop/index.tsx
@@ -1,26 +1,32 @@
-import * as React from 'react';
-
-import styles from './Backdrop.module.scss';
+import * as React from "react";
+import styled from "styled-components";
 
 interface IProps {
-    backdropClickHandler(): void,
+    backdropClickHandler(): void;
 }
 
 class Backdrop extends React.Component<IProps, {}> {
     constructor(props: IProps) {
         super(props);
-        this.clickHandler = this.clickHandler.bind(this);
-    };
+    }
 
     public render() {
-        return (
-            <div className={styles.backdrop} onClick={this.clickHandler}/>
-        );
+        return <BackdropWrapper onClick={this.clickHandler} />;
     }
 
-    private clickHandler() {
+    private clickHandler = () => {
         this.props.backdropClickHandler();
-    }
+    };
 }
 
 export default Backdrop;
+
+const BackdropWrapper = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 100;
+`;
