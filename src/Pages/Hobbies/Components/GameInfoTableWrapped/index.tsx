d6fc115c2a10e37d15b0c4c97e8fc4288fc3ef10
roottool <smart.dog.lilienthal@gmail.com>
2018-12-03 17:19:58 +0900

    読み込み完了後に表示するゲーム一覧テーブルをコンポーネントとして分離

---
+++ src/Pages/Hobbies/Components/GameInfoTableWrapped/index.tsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { withStyles } from "@material-ui/core/styles";
+
+import GameInfoContents from "../GameInfoContents";
+import TablePaginationActionsWrapped from "../TablePaginationActionWrapped";
+
+import { HobbiesState } from "../../module";
+import { ActionDispatcher } from "../../Container";
+
+const StyledTablePagination = withStyles({
+    toolbar: {
+        padding: 0
+    }
+})(TablePagination);
+
+interface IProps {
+    value: HobbiesState;
+    actions: ActionDispatcher;
+}
+
+class GameInfoTableWrapped extends Component<IProps, {}> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <Table>
+                <TableBody>
+                    {this.props.value.rows
+                        .slice(
+                            this.firstPageContent(
+                                this.props.value.page,
+                                this.props.value.rowsPerPage
+                            ),
+                            this.lastPageContent(
+                                this.props.value.page,
+                                this.props.value.rowsPerPage
+                            )
+                        )
+                        .map(row => {
+                            return (
+                                <TableRow key={row.appid}>
+                                    <TableCell
+                                        style={{
+                                            textAlign: "center"
+                                        }}
+                                    >
+                                        <GameInfoContents {...row} />
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <StyledTablePagination
+                            rowsPerPageOptions={[]}
+                            count={this.props.value.rows.length}
+                            rowsPerPage={this.props.value.rowsPerPage}
+                            labelRowsPerPage=""
+                            page={this.props.value.page}
+                            onChangePage={this.handleChangePage}
+                            ActionsComponent={TablePaginationActionsWrapped}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        );
+    }
+
+    readonly handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        page: number
+    ) => {
+        this.props.actions.changeOwnedGameInfoPage(page);
+    };
+
+    readonly firstPageContent = (currentPage: number, rowsPerPage: number) =>
+        currentPage * rowsPerPage;
+
+    readonly lastPageContent = (currentPage: number, rowsPerPage: number) =>
+        currentPage * rowsPerPage + rowsPerPage;
+}
+
+export default GameInfoTableWrapped;
--- src/Pages/Hobbies/index.tsx
+++ src/Pages/Hobbies/index.tsx
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableFooter from "@material-ui/core/TableFooter";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import {
     withStyles,
@@ -16,21 +9,14 @@ import {
     WithStyles,
     createStyles
 } from "@material-ui/core/styles";
-
-import GameInfoContents from "./Components/GameInfoContents";
-import TablePaginationActionsWrapped from "./Components/TablePaginationActionWrapped";
-
+// redux関連
 import { HobbiesState } from "./module";
 import { ActionDispatcher } from "./Container";
-
+// components
+import GameInfoTableWrapped from "./Components/GameInfoTableWrapped";
+// style
 import PageTitleWrapper from "../../Shared/Styles/PageTitleWrapper";
 import { MIN_TABLET_SIZE } from "../../Shared/Styles/StyleConstants";
-
-const StyledTablePagination = withStyles({
-    toolbar: {
-        padding: 0
-    }
-})(TablePagination);
 
 const styleSettings = (theme: Theme) =>
     createStyles({
@@ -61,56 +47,8 @@ class Hobbies extends Component<IProps, {}> {
         this.props.actions.requestFetchingUserOwnedGameInfo();
     }
 
-    private handleChangePage = (
-        event: React.MouseEvent<HTMLButtonElement> | null,
-        page: number
-    ) => {
-        this.props.actions.changeOwnedGameInfoPage(page);
-    };
-
     public render() {
         const { classes } = this.props;
-
-        const renderCompletedFetching = (
-            <Table>
-                <TableBody>
-                    {this.props.value.rows
-                        .slice(
-                            this.props.value.page *
-                                this.props.value.rowsPerPage,
-                            this.props.value.page *
-                                this.props.value.rowsPerPage +
-                                this.props.value.rowsPerPage
-                        )
-                        .map(row => {
-                            return (
-                                <TableRow key={row.appid}>
-                                    <TableCell
-                                        style={{
-                                            textAlign: "center"
-                                        }}
-                                    >
-                                        <GameInfoContents {...row} />
-                                    </TableCell>
-                                </TableRow>
-                            );
-                        })}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <StyledTablePagination
-                            rowsPerPageOptions={[]}
-                            count={this.props.value.rows.length}
-                            rowsPerPage={this.props.value.rowsPerPage}
-                            labelRowsPerPage=""
-                            page={this.props.value.page}
-                            onChangePage={this.handleChangePage}
-                            ActionsComponent={TablePaginationActionsWrapped}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        );
 
         return (
             <div>
@@ -126,7 +64,7 @@ class Hobbies extends Component<IProps, {}> {
                             <CircularProgress className={classes.progress} />
                         </div>
                     ) : (
-                        renderCompletedFetching
+                        <GameInfoTableWrapped {...this.props} />
                     )}
                 </Paper>
             </div>
