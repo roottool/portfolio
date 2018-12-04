2a181e683cbb3710480e252d2a902db9d96806ae
roottool <smart.dog.lilienthal@gmail.com>
2018-12-04 18:46:01 +0900

    PWA減点対応

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
+import * as serviceWorker from "./serviceWorker";
 
 import App from "./Container";
 
@@ -15,3 +16,8 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root") as HTMLElement
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
