a52b9286bbc54304f3f38858f97b1fbd69b7fc43
roottool <smart.dog.lilienthal@gmail.com>
2019-02-06 15:04:48 +0900

    Use .env file

--- src/sagas/hobbies.ts
+++ src/sagas/hobbies.ts
@@ -38,8 +38,8 @@ const sortOwnedGames = (ownedGames: any) => {
  * Therefore I cancel to use this function until this issue is resolved.
  */
 const fetchOwnedGamesApi = () => {
-    const apiKey = "XXX";
-    const steamId = "YYY";
+    const apiKey = process.env.REACT_APP_STEAM_API_KEY;
+    const steamId = process.env.REACT_APP_STEAM_USER_ID;
     const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&include_appinfo=1`;
 
     return axios
@@ -54,12 +54,12 @@ const fetchOwnedGamesApi = () => {
 
 export function* fetchUserOwnedGameInfo() {
     while (true) {
-    yield take(ActionNames.REQUEST_FETCH);
-        // const ownedGames = yield call(fetchOwnedGamesApi);
-        const ownedGames = contents
-    const sortedOwendGames = yield call(sortOwnedGames, ownedGames);
-    yield put(receiveFetchedUserOwnedGameInfo(sortedOwendGames));
-}
+        yield take(ActionNames.REQUEST_FETCH);
+        const ownedGames = yield call(fetchOwnedGamesApi);
+        // const ownedGames = contents
+        const sortedOwendGames = yield call(sortOwnedGames, ownedGames);
+        yield put(receiveFetchedUserOwnedGameInfo(sortedOwendGames));
+    }
 }
 
 export default function* root() {
