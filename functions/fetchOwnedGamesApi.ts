9be42c5d6b4b261d535c24081614f6109fcb08ba
roottool <smart.dog.lilienthal@gmail.com>
2019-02-06 16:34:36 +0900

    fixed from header to handler

--- functions/fetchOwnedGamesApi.ts
+++ functions/fetchOwnedGamesApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-exports.header = async (event, context) => {
+exports.handler = async (event, context) => {
     const apiKey = process.env.REACT_APP_STEAM_API_KEY;
     const steamId = process.env.REACT_APP_STEAM_USER_ID;
     const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&include_appinfo=1`;
