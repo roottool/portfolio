3e02150368044c048aa2751543a32f882eb20928
roottool <operation.versaile.roottool@gmail.com>
2021-05-23 19:24:43 +0000

    fix: Fix to pass `yarn test:check-types`

--- functions/fetchOwnedGamesApi.ts
+++ functions/fetchOwnedGamesApi.ts
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
-exports.handler = (_event, _context, callback) => {
+interface Response {
+  body: string
+  statusCode: number
+}
+
+exports.handler = (
+  _event: unknown,
+  _context: unknown,
+  callback: (context: null, response: Response) => void
+) => {
   const apiKey = process.env.REACT_APP_STEAM_API_KEY
   const steamId = process.env.REACT_APP_STEAM_USER_ID
   const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&include_appinfo=1`
