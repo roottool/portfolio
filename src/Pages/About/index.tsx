1f73107c7632959df280e80a338bfe84aa094268
roottool <operation.versatile.roottool@gmail.com>
2022-01-06 12:33:18 +0000

    feat: Convert from class to function #30

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,8 +1,5 @@
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Grid from '@material-ui/core/Grid'
-import { withStyles, Theme, WithStyles, createStyles } from '@material-ui/core/styles'
-import { Component } from 'react'
+import { Card, CardContent, Grid } from '@material-ui/core'
+import { createStyles, withStyles, type Theme, type WithStyles } from '@material-ui/core/styles'
 import { Helmet } from 'react-helmet'
 import { IconContext } from 'react-icons'
 import { FaTwitterSquare } from 'react-icons/fa'
@@ -28,54 +25,53 @@ const styleSettings = (theme: Theme) =>
     },
   })
 
-class About extends Component<WithStyles<typeof styleSettings>> {
-  constructor(props: WithStyles<typeof styleSettings>) {
-    super(props)
-  }
+const GITHUB_URL = 'https://github.com/roottool' as const
+const TWITTER_URL = 'https://twitter.com/roottool' as const
+const About = ({ classes }: WithStyles<typeof styleSettings>) => (
+  <div>
+    <PageTitleWrapper>About</PageTitleWrapper>
+    <Card className={classes.card}>
+      <CardContent>
+        <Grid container className={classes.contents} spacing={8}>
+          <Grid item xs={6}>
+            Name
+          </Grid>
+          <Grid item xs={6}>
+            roottool
+          </Grid>
+          <Grid item xs={6}>
+            Birthday
+          </Grid>
+          <Grid item xs={6}>
+            1990/8/15
+          </Grid>
+          <Grid item xs={6}>
+            <a href={GITHUB_URL}>
+              <IconContext.Provider value={{ size: '3em' }}>
+                <GoMarkGithub />
+              </IconContext.Provider>
+            </a>
+          </Grid>
+          <Grid item xs={6}>
+            <a href={TWITTER_URL}>
+              <IconContext.Provider value={{ size: '3em' }}>
+                <FaTwitterSquare />
+              </IconContext.Provider>
+            </a>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  </div>
+)
 
-  public render() {
-    const { classes } = this.props
-    return (
-      <div>
-        <Helmet>
-          <title>About - roottool&apos;s Portfolio Site</title>
-        </Helmet>
-        <PageTitleWrapper>About</PageTitleWrapper>
-        <Card className={classes.card}>
-          <CardContent>
-            <Grid container className={classes.contents} spacing={8}>
-              <Grid item xs={6}>
-                Name
-              </Grid>
-              <Grid item xs={6}>
-                roottool
-              </Grid>
-              <Grid item xs={6}>
-                Birthday
-              </Grid>
-              <Grid item xs={6}>
-                1990/8/15
-              </Grid>
-              <Grid item xs={6}>
-                <a href="https://github.com/roottool">
-                  <IconContext.Provider value={{ size: '3em' }}>
-                    <GoMarkGithub />
-                  </IconContext.Provider>
-                </a>
-              </Grid>
-              <Grid item xs={6}>
-                <a href="https://twitter.com/roottool">
-                  <IconContext.Provider value={{ size: '3em' }}>
-                    <FaTwitterSquare />
-                  </IconContext.Provider>
-                </a>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </div>
-    )
-  }
-}
+const Container = ({ classes }: WithStyles<typeof styleSettings>) => (
+  <>
+    <Helmet>
+      <title>About - roottool&apos;s Portfolio Site</title>
+    </Helmet>
+    <About classes={classes} />
+  </>
+)
 
-export default withStyles(styleSettings)(About)
+export default withStyles(styleSettings)(Container)
