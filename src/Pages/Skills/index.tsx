46ff1c8d1b3a125e419803c51db8bc3c33c4f59c
roottool <operation.versatile.roottool@gmail.com>
2022-01-07 15:31:32 +0000

    feat: Convert the Skills page to FC #30

--- src/Pages/Skills/index.tsx
+++ src/Pages/Skills/index.tsx
@@ -1,13 +1,29 @@
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles'
-import { Component } from 'react'
+import { Card, CardContent, Grid, Typography } from '@material-ui/core'
+import { createStyles, withStyles, type WithStyles } from '@material-ui/core/styles'
 import { Helmet } from 'react-helmet'
 
 import PageTitleWrapper from '../../Components/atoms/PageTitleWrapper'
 import contents from './SkillsContents.json'
+
+const Skills = ({ classes }: WithStyles<typeof styleSettings>) => (
+  <div>
+    <PageTitleWrapper>Skills</PageTitleWrapper>
+    <Grid container className={classes.container}>
+      {contents.map((item, key) => (
+        <Grid key={key} item className={classes.cardGrid} md={4} sm={6} xs={12}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography gutterBottom variant="h6">
+                {item.title}
+              </Typography>
+              <Typography variant="subtitle1">{item.text}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  </div>
+)
 
 const styleSettings = () =>
   createStyles({
@@ -24,36 +40,13 @@ const styleSettings = () =>
     },
   })
 
-class Skills extends Component<WithStyles<typeof styleSettings>> {
-  constructor(props: WithStyles<typeof styleSettings>) {
-    super(props)
-  }
+const Container = ({ classes }: WithStyles<typeof styleSettings>) => (
+  <>
+    <Helmet>
+      <title>Skills - roottool&apos;s Portfolio Site</title>
+    </Helmet>
+    <Skills classes={classes} />
+  </>
+)
 
-  public render() {
-    const { classes } = this.props
-    return (
-      <div>
-        <Helmet>
-          <title>Skills - roottool&apos;s Portfolio Site</title>
-        </Helmet>
-        <PageTitleWrapper>Skills</PageTitleWrapper>
-        <Grid container className={classes.container}>
-          {contents.map((item, key) => (
-            <Grid key={key} item className={classes.cardGrid} md={4} sm={6} xs={12}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography gutterBottom variant="h6">
-                    {item.title}
-                  </Typography>
-                  <Typography variant="subtitle1">{item.text}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    )
-  }
-}
-
-export default withStyles(styleSettings)(Skills)
+export default withStyles(styleSettings)(Container)
