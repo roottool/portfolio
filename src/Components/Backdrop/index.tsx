8b0dc8c841fd2f5d4558240b0c3638201fce3314
roottool <operation.versatile.roottool@gmail.com>
2022-01-07 14:20:23 +0000

    refactor: Convert Backdrop to FC #30

--- src/Components/Backdrop/index.tsx
+++ src/Components/Backdrop/index.tsx
@@ -1,27 +1,14 @@
-import { Component } from 'react'
 import styled from 'styled-components'
 
-interface IProps {
+interface Props {
   backdropClickHandler(): void
 }
 
-class Backdrop extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props)
-  }
+const Backdrop = ({ backdropClickHandler }: Props) => (
+  <div aria-hidden="true" onClick={backdropClickHandler} />
+)
 
-  public render(): JSX.Element {
-    return <BackdropWrapper onClick={this.clickHandler} />
-  }
-
-  private clickHandler = () => {
-    this.props.backdropClickHandler()
-  }
-}
-
-export default Backdrop
-
-const BackdropWrapper = styled.div`
+const BackdropWrapper = styled(Backdrop)`
   position: fixed;
   width: 100%;
   height: 100%;
@@ -30,3 +17,5 @@ const BackdropWrapper = styled.div`
   background: rgba(0, 0, 0, 0.3);
   z-index: 100;
 `
+
+export default BackdropWrapper
