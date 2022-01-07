41c3a1e57ea4a1be7d561b70b3d025ee62fdf3d2
roottool <operation.versatile.roottool@gmail.com>
2022-01-07 15:25:49 +0000

    feat: Convert the Home page to FC #30

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,30 +1,15 @@
-import { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 
 import { MAX_SMARTPHONE_SIZE, MIN_TABLET_SIZE } from '../../Shared/Styles/StyleConstants'
 
-class Home extends Component {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  constructor(props: {} | Readonly<{}>) {
-    super(props)
-  }
-
-  public render(): JSX.Element {
-    return (
-      <div>
-        <Helmet>
-          <title>roottool&apos;s Portfolio Site</title>
-        </Helmet>
-        <TopPageTitleWrapper>Welcome to roottool&apos;s portfolio site!</TopPageTitleWrapper>
-        <p>&quot;Why do it yourself when robots do it better?&quot;</p>
-        <p>- echo -</p>
-      </div>
-    )
-  }
-}
-
-export default Home
+const Home = () => (
+  <div>
+    <TopPageTitleWrapper>Welcome to roottool&apos;s portfolio site!</TopPageTitleWrapper>
+    <p>&quot;Why do it yourself when robots do it better?&quot;</p>
+    <p>- echo -</p>
+  </div>
+)
 
 const TopPageTitleWrapper = styled.h1`
   @media (max-width: ${MAX_SMARTPHONE_SIZE}px) {
@@ -37,3 +22,14 @@ const TopPageTitleWrapper = styled.h1`
     margin-top: 40vh;
   }
 `
+
+const Container = () => (
+  <>
+    <Helmet>
+      <title>roottool&apos;s Portfolio Site</title>
+    </Helmet>
+    <Home />
+  </>
+)
+
+export default Container
