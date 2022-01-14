af1eb4ebb92f77433cb6a65cff69e2c39e666dd1
roottool <operation.versatile.roottool@gmail.com>
2022-01-14 23:54:10 +0900

    refactor: Convert the Navbar to FC #30

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,13 +1,38 @@
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles'
-import { Component } from 'react'
+import { AppBar, Toolbar } from '@material-ui/core'
+import { createStyles, withStyles, type WithStyles } from '@material-ui/core/styles'
 import { IconContext } from 'react-icons'
 import { MdMenu } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { MAX_SMARTPHONE_SIZE, MIN_TABLET_SIZE } from '../../Shared/Styles/StyleConstants'
+
+interface Props {
+  drawToggleClickHandler: () => void
+}
+interface NavbarComponentProps extends WithStyles<typeof styleSettings> {
+  handleClick: Props['drawToggleClickHandler']
+}
+
+const NavbarComponent = ({ classes: { root }, handleClick }: NavbarComponentProps) => (
+  <AppBar className={root} position={'static'}>
+    <Toolbar>
+      <MenuIconWrapper onClick={handleClick}>
+        <IconContext.Provider value={{ color: 'white', size: '1.5em' }}>
+          <MdMenu />
+        </IconContext.Provider>
+      </MenuIconWrapper>
+      <TitleWrapper to="/">roottool&apos;s portfolio</TitleWrapper>
+      <GrowWrapper />
+      <NavigationItemsWrapper>
+        <LinkWrapper to="/about">about</LinkWrapper>
+        <LinkWrapper to="/works">Works</LinkWrapper>
+        <LinkWrapper to="/skills">Skills</LinkWrapper>
+        <LinkWrapper to="/hobbies">Hobbies</LinkWrapper>
+      </NavigationItemsWrapper>
+    </Toolbar>
+  </AppBar>
+)
 
 const styleSettings = () =>
   createStyles({
@@ -18,47 +43,7 @@ const styleSettings = () =>
       background: '#df4848',
     },
   })
-
-interface IProps extends WithStyles<typeof styleSettings> {
-  drawToggleClickHandler(): void
-}
-
-class Navbar extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props)
-  }
-
-  public render() {
-    const { classes } = this.props
-    return (
-      <div>
-        <AppBar className={classes.root} position={'static'}>
-          <Toolbar>
-            <MenuIconWrapper onClick={this.clickHandler}>
-              <IconContext.Provider value={{ color: 'white', size: '1.5em' }}>
-                <MdMenu />
-              </IconContext.Provider>
-            </MenuIconWrapper>
-            <TitleWrapper to="/">roottool&apos;s portfolio</TitleWrapper>
-            <GrowWrapper />
-            <NavigationItemsWrapper>
-              <LinkWrapper to="/about">about</LinkWrapper>
-              <LinkWrapper to="/works">Works</LinkWrapper>
-              <LinkWrapper to="/skills">Skills</LinkWrapper>
-              <LinkWrapper to="/hobbies">Hobbies</LinkWrapper>
-            </NavigationItemsWrapper>
-          </Toolbar>
-        </AppBar>
-      </div>
-    )
-  }
-
-  private clickHandler = () => {
-    this.props.drawToggleClickHandler()
-  }
-}
-
-export default withStyles(styleSettings)(Navbar)
+const StyledNavbarComponent = withStyles(styleSettings)(NavbarComponent)
 
 const MenuIconWrapper = styled.div`
   @media (min-width: ${MIN_TABLET_SIZE}px) {
@@ -97,3 +82,9 @@ const LinkWrapper = styled(Link)`
   text-decoration: none;
   padding: 16px 1rem;
 `
+
+const Navbar = ({ drawToggleClickHandler }: Props) => (
+  <StyledNavbarComponent handleClick={drawToggleClickHandler} />
+)
+
+export default Navbar
