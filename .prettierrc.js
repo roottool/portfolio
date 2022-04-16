f1a5ac55facf182878df7bfdc8156b609f1e712d
roottool <operation.versatile.roottool@gmail.com>
2022-04-17 06:25:48 +0900

    Update some config (#60)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+}
+
+module.exports = config
