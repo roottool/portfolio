722a1f1ece52b0722291e9328ff97a59fef2eb4d
roottool <operation.versatile.roottool@gmail.com>
2022-06-16 19:12:32 +0000

    chore: Update `tsconfig.json`

--- src/pages/api/fetchOwnedGames.ts
+++ src/pages/api/fetchOwnedGames.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import type { OwnedGamesResponse } from '@/utils/types'
 
-const API_KEY = process.env.REACT_APP_STEAM_API_KEY
-const STEAM_ID = process.env.REACT_APP_STEAM_USER_ID
+const API_KEY = process.env['REACT_APP_STEAM_API_KEY']
+const STEAM_ID = process.env['REACT_APP_STEAM_USER_ID']
 const URL = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${API_KEY}&steamid=${STEAM_ID}&include_appinfo=1`
 
 const fetchOwnedGamesHandler = (_req: NextApiRequest, res: NextApiResponse) =>
