5624073ff40041a7db98573ace40dffe7cbc3cb5
roottool <operation.versatile.roottool@gmail.com>
2022-07-24 23:34:29 +0900

    chore: Update some config

--- jest.config.js
+++ jest.config.js
@@ -7,14 +7,12 @@ const createJestConfig = nextJest({
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 const customJestConfig = {
-  // jest.setup.jsを作成する場合のみ定義。
+  testEnvironment: 'jest-environment-jsdom',
   // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    // aliasを定義 （tsconfig.jsonのcompilerOptions>pathsの定義に合わせる）
     '^@/(.*)$': '<rootDir>/src/$1',
     '^$/(.*)$': '<rootDir>/public/$1',
   },
-  testEnvironment: 'jest-environment-jsdom',
 }
 
 module.exports = createJestConfig(customJestConfig)
