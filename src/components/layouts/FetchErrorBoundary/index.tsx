59d5672766a9632a3bce2aadf5c2fdf9779139e0
roottool <operation.versatile.roottool@gmail.com>
2023-06-28 22:05:21 +0000

    feat: Remove Typography

--- src/components/layouts/FetchErrorBoundary/index.tsx
+++ src/components/layouts/FetchErrorBoundary/index.tsx
@@ -1,12 +1,7 @@
-import { Typography } from '@material-ui/core'
 import type { PropsWithChildren } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
-const FetchErrorBoundaryPresenter = () => (
-  <div role="alert">
-    <Typography variant="subtitle1">読み込みに失敗しました</Typography>
-  </div>
-)
+const FetchErrorBoundaryPresenter = () => <p role="alert">読み込みに失敗しました</p>
 
 const FetchErrorBoundary = ({ children }: PropsWithChildren) => (
   <ErrorBoundary FallbackComponent={FetchErrorBoundaryPresenter}>{children}</ErrorBoundary>
