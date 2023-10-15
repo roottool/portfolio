5fe50072e41b0f30cf15dcec2389daa15c48e1a5
roottool <operation.versatile.roottool@gmail.com>
2023-10-15 06:55:39 +0000

    refactor: Change module name

--- src/hooks/useFetchSuspense.ts
+++ src/hooks/useFetchSuspense.ts
@@ -1,9 +1,9 @@
-import useSwr from 'swr'
+import useSWR from 'swr'
 
 import fetcher from '@/libs/fetcher'
 
 const useFetchSuspense = <T, FetchError = unknown>(url: string) =>
-	useSwr<T, FetchError>(url, fetcher<T>, {
+	useSWR<T, FetchError>(url, fetcher<T>, {
 		suspense: true,
 	})
 
