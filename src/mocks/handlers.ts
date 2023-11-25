c7d79b69fbacf93c7af618f121510950c548595b
roottool <operation.versatile.roottool@gmail.com>
2023-11-25 16:00:12 +0000

    feat: Upgrade msw to v2

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,8 @@
 // src/mocks/handlers.js
-import { rest } from 'msw'
+import { HttpResponse, http } from 'msw'
 
 import OwnedGames from './OwnedGames.json'
-import type { OwnedGame, OwnedGamesApiResponse } from '@/libs/steam/api'
+import type { OwnedGame } from '@/libs/steam/api'
 import { GET_OWNED_GAMES_API_URL_WITHOUT_QUERY } from '@/libs/steam/construct'
 import convertPlayTimeToHours from '@/libs/steam/convertPlayTimeToHours'
 import {
@@ -13,26 +13,23 @@ import sortOwnedGames from '@/libs/steam/sortOwnedGames'
 import type { FetchOwnedGamesResponse } from '@/pages/api/fetchOwnedGames/type'
 
 export const handlers = [
-	rest.get<OwnedGamesApiResponse>(
-		GET_OWNED_GAMES_API_URL_WITHOUT_QUERY,
-		(_req, res, ctx) => {
-			return res(ctx.json(OwnedGames))
-		},
-	),
-	rest.get<FetchOwnedGamesResponse>(
-		'/api/fetchOwnedGames',
-		(_req, res, ctx) => {
-			const { game_count, games } = OwnedGames.response
-			const ownedGames = sortOwnedGames(games as OwnedGame[]).map(
-				({ appid, name, playtime_forever }) => ({
-					appId: appid,
-					appName: name,
-					appUrl: generateSteamAppUrl(appid),
-					bannerUrl: generateSteamAppBannerUrl(appid),
-					playTimeInHours: convertPlayTimeToHours(playtime_forever),
-				}),
-			)
-			return res(ctx.json({ game_count, ownedGames }))
-		},
-	),
+	http.get(GET_OWNED_GAMES_API_URL_WITHOUT_QUERY, () => {
+		return HttpResponse.json(OwnedGames)
+	}),
+	http.get('/api/fetchOwnedGames', () => {
+		const { game_count, games } = OwnedGames.response
+		const ownedGames = sortOwnedGames(games as OwnedGame[]).map(
+			({ appid, name, playtime_forever }) => ({
+				appId: appid,
+				appName: name,
+				appUrl: generateSteamAppUrl(appid),
+				bannerUrl: generateSteamAppBannerUrl(appid),
+				playTimeInHours: convertPlayTimeToHours(playtime_forever),
+			}),
+		)
+		return HttpResponse.json<FetchOwnedGamesResponse>({
+			game_count,
+			ownedGames,
+		})
+	}),
 ]
