3d5c67a829ed27fcd9c4e017b79e1b7164bc7dcb
roottool <operation.versatile.roottool@gmail.com>
2025-08-24 18:16:55 +0900

    Code formatting improvements and editorconfig adjustments (#2163)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,10 +3,19 @@ const config = {
 	semi: false,
 	singleQuote: true,
 	useTabs: true,
+	tabWidth: 2,
 	plugins: ['@trivago/prettier-plugin-sort-imports'],
 	importOrder: ['^next', '^react', '<THIRD_PARTY_MODULES>', '^(@|[.]{1,2})/'],
 	importOrderSeparation: true,
 	importOrderSortSpecifiers: true,
+	overrides: [
+		{
+			files: ['*.md', '*.json', '*.yml', '*.yaml'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
 }
 
 export default config
