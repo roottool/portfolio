3281177627ac906e683e444120b030eabdb2b4d6
roottool <operation.versatile.roottool@gmail.com>
2025-11-22 13:18:04 +0900

    style: Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, envField } from 'astro/config';
-
-
-
-
-
-
-
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, envField } from 'astro/config'
 
 export default defineConfig({
 	output: 'static',
